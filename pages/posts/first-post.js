0610b5ba6b7e4fa67983e313d2ceeba153f7cc4f
nakyungmi <na3anita@naver.com>
2021-04-12 00:51:25 +0900

    Nakyungmi

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+
+import Link from 'next/link'
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Name: Kyungmi NA Hobby:Badminton</h1>
+      <h2>
+        <Link href="/">
+          <a>Let me show you the page that I made</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
